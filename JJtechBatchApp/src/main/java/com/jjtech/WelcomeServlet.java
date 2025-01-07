4f61d1fab297bcf041fbe9a4e04b5777321c4722
mawuena <christopher.askademia@gmail.com>
2025-01-06 20:57:49 -0600

    update webpage

--- JJtechBatchApp/src/main/java/com/jjtech/WelcomeServlet.java
+++ JJtechBatchApp/src/main/java/com/jjtech/WelcomeServlet.java
@@ -14,7 +14,7 @@ public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Welcome to JJtech Model Batch DevOps complete ci-cd Git, Maven, Jenkins, Nexus, SonarQube Integration, after debugging</h1>");
+        response.getWriter().println("<h1>Welcome to JJtech Model Batch DevOps complete ci-cd Git, Maven, Jenkins, Nexus, SonarQube Integration, after debugging, We made it work!!!!!</h1>");
     }
 }
 
